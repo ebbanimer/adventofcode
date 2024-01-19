5665493c55154a02cfbd769fcd4c3308ba20a8ca
ebbanimer <76969012+ebbanimer@users.noreply.github.com>
2024-01-19 16:12:43 +0100

    done with day 2

--- Day2/src/main/java/Main.java
+++ Day2/src/main/java/Main.java
@@ -2,24 +2,12 @@ import java.io.*;
 import java.util.*;
 
 import static java.lang.Math.max;
-import static java.lang.Math.round;
 
+/**
+ * Day 2 of advent of code.
+ * @author Ebba Nimér
+ */
 public class Main {
-
-    /**
-     * Part 2;
-     * In each game; what is the fewest number of cubes of each color that could have been in the bag to make the game
-     * possible?
-     *
-     * This means;
-     * Check the max number of blue, red, and green for each game-round.
-     *
-     * Power of a set; red * green * blue
-     *
-     * Task;
-     * For each game, find the power and sum all of them.
-     * @param args
-     */
 
     private static int maxRed;
     private static int maxBlue;
@@ -50,87 +38,43 @@ public class Main {
      * @return The ID if it's a valid game.
      */
     private static int getValueFromRound(String line){
+        // If part 2, reset the max-values.
         maxRed = 0;
         maxGreen = 0;
         maxBlue = 0;
+
         String[] parts = line.split(":", 2);
         if (parts.length != 2) return 0;
 
         String gameName = parts[0].trim();
         String round = parts[1];
 
+        // Part 1; get the game ID if the game is possible
         //if (!checkIfValidRound(round)) return 0;
-        //else return getPowerSet(round);
+        //else return Integer.parseInt(gameName.replaceAll("[^0-9]", ""));
+
+        // Part 2; get the power set of the game; max red, max blue, and max green.
         return getPowerSet(round);
-
-        // Part 2;
-        // return the power set and reset maxRed, maxGreen, maxBlue
-
-        //return Integer.parseInt(gameName.replaceAll("[^0-9]", ""));
-    }
-
-    private static int getPowerSet(String round){
-
-        // split the round using;
-        // split the
-        // power set = maxRed * maxBlue * maxGreen
-        // Loop through the round
-        // get the value for each color
-        String[] hej = round.split(";");
-        for (String s : hej){
-            s = s.trim();
-            String[] baj = s.split(",");
-            for (String b : baj){
-                b = b.trim();
-                splitValueAndColor(b);
-                //System.out.println(b);
-            }
-        }
-
-        int powerset = maxBlue * maxRed * maxGreen;
-
-        System.out.println("Maxred: " + maxRed);
-
-
-        /*Arrays.stream(round.split(";")) // here, the ";" is cut out
-                .map(String::trim)
-                .flatMap(data -> Arrays.stream(data.split(","))
-                        .map(String::trim)
-                        .map(Main::splitValueAndColor));*/
-
-
-
-        //hej1.forEach(sequence -> sequence.split(","));
-
-
-        //colVal.forEach(System.out::println);
-        //System.out.println(colVal);
-        //hej1.forEach(Main::splitValueAndColor);
-
-        //System.out.println("Max red: " + maxRed);
-
-
-
-
-        return powerset;
     }
 
     /**
-     * Check if game-round is valid.
-     * @return ID of the game if valid.
-     * @param data data
+     * Calculates the power set of the round.
+     * @param round The game-round.
+     * @return The power set.
      */
-    /*private static int getPowerSetForRound(String data) {
-
-        return Arrays.stream(data.split(";"))
+    private static int getPowerSet(String round) {
+        Arrays.stream(round.split(";"))
                 .map(String::trim)
-                .allMatch(round -> Arrays.stream(round.split(","))
-                        .map(String::trim)
-                        .allMatch(Main::checkIfValidColorAndValue));
-    }*/
+                .flatMap(s -> Arrays.stream(s.split(",")))
+                .map(String::trim)
+                .forEach(Main::splitValueAndColor);
+
+        return maxBlue * maxRed * maxGreen;
+    }
 
     /**
-     * Splits the string to color and value. Get's the value.
+     * Part 2.
+     * Splits the string to color and value. Gets the amount.
      * @param str String containing amount of cubes and color.
      */
     private static void splitValueAndColor(String str) {
@@ -138,33 +82,29 @@ public class Main {
         try {
             int val = Integer.parseInt(parts[0]);
             String color = parts[1];
-            //System.out.println("Val: " + val + ", color: " + color);
             getValueForColor(color, val);
         } catch (NumberFormatException ignored) {
         }
     }
 
+    /**
+     * Part 2
+     * Gets the amount of cubes for corresponding color.
+     * @param color The color of the cubes.
+     * @param value The amount of cubes.
+     */
     private static void getValueForColor(String color, int value){
         switch (color) {
-            case "red" -> {
-                maxRed = max(value, maxRed);
-            }
-            // maxRed = max(value, maxRed)
-            case "blue" -> {
-                maxBlue = max(value, maxBlue);
-            }
-            // maxBlue = max(value, maxBlue)
-            case "green" -> {
-                maxGreen = max(value, maxGreen);
-            }
-            default -> {
-            }
+            case "red" -> maxRed = max(value, maxRed);
+            case "blue" -> maxBlue = max(value, maxBlue);
+            case "green" -> maxGreen = max(value, maxGreen);
         }
     }
 
     // PART 1
 
     /**
+     * Part 1
      * Check if game-round is valid.
      * @return ID of the game if valid.
      * @param data data
@@ -178,6 +118,7 @@ public class Main {
     }
 
     /**
+     * Part 1
      * Checks if the color and value are valid.
      * @param str String containing amount of cubes and color.
      * @return Whether it is valid or not.
@@ -196,6 +137,7 @@ public class Main {
     }
 
     /**
+     * Part 1
      * Verifies if the round is valid by checking amount of cubes and the color.
      * @param color The color.
      * @param val Amount of cubes.
